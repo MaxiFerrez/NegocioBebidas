c8936aab153a2e3cd59938cb2d54089cce49cea2
MaxiFerrez <maxiferrez@gmail.com>
2023-05-22 11:49:16 -0300

    Se agrego el estilo de la pagina principal

---
+++ src/app/bebidas.ts
@@ -0,0 +1,5 @@
+export class Bebida {
+    id: number;
+    nombre: string;
+    permite: number;
+  }
--- src/app/contenido-web/contenido-web.component.ts
+++ src/app/contenido-web/contenido-web.component.ts
@@ -1,5 +1,6 @@
 import { Component,Input,OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Bebida } from '../bebidas';
 
 @Component({
   selector: 'app-contenido-web',
@@ -8,8 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class ContenidoWebComponent {
-  bebidas = ['uno','dos','tres'];
+   
   datoRecibido: number;
+
+  lista: Bebida[] = [
+    { id: 1, nombre: 'Licor', permite: 0},
+    { id: 2, nombre: 'Fernet', permite: 0},
+    { id: 3, nombre: 'Coca-Cola', permite: 1},
+    /* { id: 4, nombre: 'Jugo', permite: 1},
+    { id: 5, nombre: 'Votka', permite: 0},
+    { id: 6, nombre: 'Agua Tonica', permite: 1}, */
+  ];
 
   
   constructor(private route: ActivatedRoute) {}
