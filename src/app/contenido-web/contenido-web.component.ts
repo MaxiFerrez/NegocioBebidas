81f92b14684f98f888fee96420bca7a27ecc36e1
MaxiFerrez <maxiferrez@gmail.com>
2023-05-21 21:50:09 -0300

    prueba nuevo pull

--- src/app/contenido-web/contenido-web.component.ts
+++ src/app/contenido-web/contenido-web.component.ts
@@ -1,5 +1,4 @@
 import { Component,Input,OnInit } from '@angular/core';
-//import { FormInicialComponent } from '../form-inicial/form-inicial.component';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,21 +10,12 @@ import { ActivatedRoute } from '@angular/router';
 export class ContenidoWebComponent {
   bebidas = ['uno','dos','tres'];
   datoRecibido: number;
-  //miVariableBooleana: boolean;
+
   
   constructor(private route: ActivatedRoute) {}
-  //@Input() mayor_de_edad:boolean;
-
    ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.datoRecibido = params['dato'];
-
-
-
-    /* this.route.paramMap.subscribe(params => {
-       this.miVariableBooleana = Boolean(params.get('variable'));
-       this.miVariableBooleana = this.miVariableBooleana; */
-      // Utiliza el valor en el componente de destino según tus necesidades
     });
   } 
 
